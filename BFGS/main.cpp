50dff18b0d129db129071eb7272610464bc8625d
gnishida <gnishida@purdue.edu>
2015-04-15 18:35:07 -0400

    update

--- BFGS/main.cpp
+++ BFGS/main.cpp
@@ -1,5 +1,9 @@
 ﻿/**
- * L-BFGS のテスト
+ * alglibを使った、L-BFGS のテスト
+ *
+ * 以下の関数の最小値を求める。
+ * f(x) = 100*(x0+3)^4 + (x1-3)^4 + (x2+1)^2
+ * 解は、[-3, 3, -1]。
  *
  * @author Gen Nishida
  * @date 4/15/2015
@@ -12,24 +16,20 @@
 #include "optimization.h"
 
 using namespace alglib;
-void function1_grad(const real_1d_array &x, double &func, real_1d_array &grad, void *ptr) 
-{
+void function1_grad(const real_1d_array &x, double &func, real_1d_array &grad, void *ptr)  {
     //
-    // this callback calculates f(x0,x1) = 100*(x0+3)^4 + (x1-3)^4
-    // and its derivatives df/d0 and df/dx1
+    // 関数の値を計算する： f(x0,x1) = 100*(x0+3)^4 + (x1-3)^4 + (x2+1)^2
+    // また、偏微分も計算する：df/dx0, df/dx1, df/dx2
     //
-    func = 100*pow(x[0]+3,4) + pow(x[1]-3,4);
+    func = 100*pow(x[0]+3,4) + pow(x[1]-3,4) + pow(x[2]+1,2);
     grad[0] = 400*pow(x[0]+3,3);
     grad[1] = 4*pow(x[1]-3,3);
+	grad[2] = 2*(x[2]+1);
 }
 
-int main(int argc, char **argv)
-{
-    //
-    // This example demonstrates minimization of f(x,y) = 100*(x+3)^4+(y-3)^4
-    // using LBFGS method.
-    //
-    real_1d_array x = "[0,0]";
+int main(int argc, char **argv) {
+    real_1d_array x = "[0,0,0]";		// 初期値
+
     double epsg = 0.0000000001;
     double epsf = 0;
     double epsx = 0;
@@ -42,7 +42,7 @@ int main(int argc, char **argv)
     alglib::minlbfgsoptimize(state, function1_grad);
     minlbfgsresults(state, x, rep);
 
-    printf("%d\n", int(rep.terminationtype)); // EXPECTED: 4
-    printf("%s\n", x.tostring(2).c_str()); // EXPECTED: [-3,3]
+    printf("termination type: %d\n", int(rep.terminationtype));
+    printf("solution: %s\n", x.tostring(2).c_str());
     return 0;
 }
